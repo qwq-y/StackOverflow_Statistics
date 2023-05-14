e673a3bcf98a946bd5bc153dca998ea0feef3ef7
qwq-y <2090078073@qq.com>
2023-05-14 20:36:15 +0800

    数据库中加了user, comment, answer等表，在原先爬取question的基础上，根据questionId进一步爬取相关信息，储存在数据库中，便于后续统计。不过现在还有个bug，就是对于用户不存在等情况还没处理，所以跑不动，先存个档。数据库建表语句和爬取的数据示例放在了单独的txt中。

--- src/main/java/com/example/utils/StackOverflowDataCollector.java
+++ src/main/java/com/example/utils/StackOverflowDataCollector.java
@@ -1,16 +1,23 @@
 package com.example.utils;
 
-import java.io.*;
-import java.net.*;
-import java.nio.charset.StandardCharsets;
-import java.sql.*;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Types;
+import java.util.Scanner;
 import java.util.zip.GZIPInputStream;
-import org.json.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class StackOverflowDataCollector {
-//  private static final String API_KEY = "YOUR_API_KEY_HERE";
+
+  private static final String API_KEY = "YP8OChnmFH4yoDkDMzJUjQ((";
   private static final String BASE_URL = "https://api.stackexchange.com/2.3";
   private static final int PAGE_SIZE = 100;
   private static final String TAG = "java";
@@ -21,7 +28,6 @@ public class StackOverflowDataCollector {
   private static Connection conn = null;
 
   public static void main(String[] args) throws Exception {
-    // 数据库连接
     File file = new File("C:\\Java2Proj\\src\\main\\resources\\templates\\databaseInfo\\dbInfo");
     Scanner scanner = new Scanner(file);
     DB_URL = scanner.nextLine();
@@ -30,32 +36,60 @@ public class StackOverflowDataCollector {
     scanner.close();
     conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
 
-    // 存储questions
-    int page = 1;
-    int count = 0;
-    while (page <= MAX_PAGES) {
-      JSONArray items = getQuestions(page);
-      for (int i = 0; i < items.length(); i++) {
-        JSONObject item = items.getJSONObject(i);
-        storeQuestion(item);
-        count++;
-        System.out.println("Question #" + count + ": " + item.getString("title"));
-      }
-      page++;
-      // 添加delay防止rate limit
-      Thread.sleep(1000);
-    }
+    // 会在getAndStoreQuestions()方法内调用其他的“getAndStore方法”
+    getAndStoreQuestions();
 
     conn.close();
   }
 
-  private static JSONArray getQuestions(int page) throws Exception {
-    String url = BASE_URL + "/questions?page=" + page + "&pagesize=" + PAGE_SIZE + "&order=desc&sort=votes&tagged=" + TAG + "&site=stackoverflow" /*+ "&key="  + API_KEY*/;
-    System.out.println("url: " + url);
+  private static void getAndStoreAnswers(long quesitonId) throws Exception {
+    String url = "https://api.stackexchange.com/2.3/questions/" + quesitonId
+        + "/answers?site=stackoverflow&key=" + API_KEY;
     String json = getUrlContents(url);
-
     JSONObject obj = new JSONObject(json);
-    return obj.getJSONArray("items");
+    JSONArray items = obj.getJSONArray("items");
+    for (int i = 0; i < items.length(); i++) {
+      JSONObject item = items.getJSONObject(i);
+      storeAnswers(item);
+    }
+  }
+
+  private static void getAndStoreComments(long questionId) throws Exception {
+    String url = "https://api.stackexchange.com/2.3/questions/" + questionId
+        + "/comments?site=stackoverflow&key=" + API_KEY;
+    String json = getUrlContents(url);
+    JSONObject obj = new JSONObject(json);
+    JSONArray items = obj.getJSONArray("items");
+    for (int i = 0; i < items.length(); i++) {
+      JSONObject item = items.getJSONObject(i);
+      storeComments(item);
+    }
+  }
+
+  private static void getAndStoreQuestions() throws Exception {
+    int page = 1;
+    int count = 0;
+    while (page <= MAX_PAGES) {
+      String url = BASE_URL + "/questions?page=" + page + "&pagesize=" + PAGE_SIZE
+          + "&order=desc&sort=votes&tagged=" + TAG + "&site=stackoverflow&key=" + API_KEY;
+      String json = getUrlContents(url);
+      JSONObject obj = new JSONObject(json);
+      JSONArray items = obj.getJSONArray("items");
+      for (int i = 0; i < items.length(); i++) {
+        JSONObject item = items.getJSONObject(i);
+        storeQuestions(item);
+        count++;
+        System.out.println("question #" + count + ": " + item.getString("title"));
+
+        long questionId = item.getLong("question_id");
+        getAndStoreAnswers(questionId);
+        getAndStoreComments(questionId);
+      }
+      page++;
+
+      // 添加delay防止rate limit
+      Thread.sleep(1000);
+    }
   }
 
   private static String getUrlContents(String urlString) throws Exception {
@@ -81,15 +115,166 @@ public class StackOverflowDataCollector {
     String line;
     StringBuilder sb = new StringBuilder();
     while ((line = reader.readLine()) != null) {
-      System.out.println(line);
       sb.append(line);
     }
     reader.close();
     return sb.toString();
   }
 
-  private static void storeQuestion(JSONObject item) throws Exception {
-    PreparedStatement pstmt = conn.prepareStatement("INSERT INTO question VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+  private static void storeUser(JSONObject item) throws Exception {
+    PreparedStatement pstmt = conn.prepareStatement(
+        "INSERT INTO user_ VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    try {
+      pstmt.setLong(1, item.getLong("account_id"));
+
+      if (item.has("reputation")) {
+        pstmt.setLong(2, item.getLong("reputation"));
+      } else {
+        pstmt.setNull(2, Types.BIGINT);
+      }
+      if (item.has("user_id")) {
+        pstmt.setLong(3, item.getLong("user_id"));
+      } else {
+        pstmt.setNull(3, Types.BIGINT);
+      }
+      if (item.has("user_type")) {
+        pstmt.setString(4, item.getString("user_type"));
+      } else {
+        pstmt.setNull(4, Types.VARCHAR);
+      }
+      if (item.has("accept_rate")) {
+        pstmt.setInt(5, item.getInt("accept_rate"));
+      } else {
+        pstmt.setNull(5, Types.INTEGER);
+      }
+      if (item.has("profile_image")) {
+        pstmt.setString(6, item.getString("profile_image"));
+      } else {
+        pstmt.setNull(6, Types.VARCHAR);
+      }
+      if (item.has("display_name")) {
+        pstmt.setString(7, item.getString("display_name"));
+      } else {
+        pstmt.setNull(7, Types.VARCHAR);
+      }
+      if (item.has("link")) {
+        pstmt.setString(8, item.getString("link"));
+      } else {
+        pstmt.setNull(8, Types.VARCHAR);
+      }
+    } catch (Exception e) {
+      System.out.println("----------------user skipped----------------");
+    }
+  }
+
+  private static void storeComments(JSONObject item) throws Exception {
+    PreparedStatement pstmt = conn.prepareStatement(
+        "INSERT INTO comment VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+
+    if (item.has("owner")) {
+      JSONObject owner = item.getJSONObject("owner");
+      pstmt.setLong(1, owner.getLong("account_id"));
+      storeUser(owner);
+    } else {
+      pstmt.setNull(1, Types.BIGINT);
+    }
+    if (item.has("reply_to_user")) {
+      JSONObject replyToUser = item.getJSONObject("reply_to_user");
+      pstmt.setLong(1, replyToUser.getLong("account_id"));
+      storeUser(replyToUser);
+    } else {
+      pstmt.setNull(2, Types.BIGINT);
+    }
+    if (item.has("edited")) {
+      pstmt.setBoolean(3, item.getBoolean("edited"));
+    } else {
+      pstmt.setNull(3, Types.BOOLEAN);
+    }
+    if (item.has("score")) {
+      pstmt.setLong(4, item.getLong("score"));
+    } else {
+      pstmt.setNull(4, Types.BIGINT);
+    }
+    if (item.has("creation_date")) {
+      pstmt.setLong(5, item.getLong("creation_date"));
+    } else {
+      pstmt.setNull(5, Types.BIGINT);
+    }
+    if (item.has("post_id")) {
+      pstmt.setLong(6, item.getLong("post_id"));
+    } else {
+      pstmt.setNull(6, Types.BIGINT);
+    }
+
+    pstmt.setLong(7, item.getLong("comment_id"));
+
+    if (item.has("content_license")) {
+      pstmt.setString(8, item.getString("content_license"));
+    } else {
+      pstmt.setNull(8, Types.VARCHAR);
+    }
+  }
+
+  private static void storeAnswers(JSONObject item) throws Exception {
+    PreparedStatement pstmt = conn.prepareStatement(
+        "INSERT INTO answer VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+    if (item.has("owner")) {
+      JSONObject owner = item.getJSONObject("owner");
+      // TODO
+      System.out.println("---------------------------------------");
+      System.out.println(owner.toString());
+      System.out.println("---------------------------------------");
+      long test = owner.getLong("account_id");
+      System.out.println(test);
+      pstmt.setLong(1, owner.getLong("account_id"));
+      storeUser(owner);
+    } else {
+      pstmt.setNull(1, Types.BIGINT);
+    }
+    if (item.has("is_accepted")) {
+      pstmt.setBoolean(2, item.getBoolean("is_accepted"));
+    } else {
+      pstmt.setNull(2, Types.BOOLEAN);
+    }
+    if (item.has("score")) {
+      pstmt.setLong(3, item.getLong("score"));
+    } else {
+      pstmt.setNull(3, Types.BIGINT);
+    }
+    if (item.has("last_activity_date")) {
+      pstmt.setLong(4, item.getLong("last_activity_date"));
+    } else {
+      pstmt.setNull(4, Types.BIGINT);
+    }
+    if (item.has("last_edit_date")) {
+      pstmt.setLong(5, item.getLong("last_edit_date"));
+    } else {
+      pstmt.setNull(5, Types.BIGINT);
+    }
+    if (item.has("creation_date")) {
+      pstmt.setLong(6, item.getLong("creation_date"));
+    } else {
+      pstmt.setNull(6, Types.BIGINT);
+    }
+
+    pstmt.setLong(7, item.getLong("answer_id"));
+
+    if (item.has("question_id")) {
+      pstmt.setLong(8, item.getLong("question_id"));
+    } else {
+      pstmt.setNull(8, Types.BIGINT);
+    }
+    if (item.has("content_license")) {
+      pstmt.setString(9, item.getString("content_license"));
+    } else {
+      pstmt.setNull(9, Types.VARCHAR);
+    }
+  }
+
+  private static void storeQuestions(JSONObject item) throws Exception {
+    PreparedStatement pstmt = conn.prepareStatement(
+        "INSERT INTO question VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
     try {
       if (item.has("tags")) {
@@ -97,55 +282,48 @@ public class StackOverflowDataCollector {
       } else {
         pstmt.setNull(1, Types.VARCHAR);
       }
-
       if (item.has("owner")) {
-        pstmt.setString(2, item.getJSONObject("owner").toString());
+        JSONObject owner = item.getJSONObject("owner");
+        pstmt.setLong(1, owner.getLong("account_id"));
+        storeUser(owner);
       } else {
-        pstmt.setNull(2, Types.VARCHAR);
+        pstmt.setNull(2, Types.BIGINT);
       }
-
       if (item.has("is_answered")) {
         pstmt.setBoolean(3, item.getBoolean("is_answered"));
       } else {
         pstmt.setNull(3, Types.BOOLEAN);
       }
-
       if (item.has("view_count")) {
         pstmt.setLong(4, item.getLong("view_count"));
       } else {
         pstmt.setNull(4, Types.BIGINT);
       }
-
       if (item.has("protected_date")) {
         pstmt.setLong(5, item.getLong("protected_date"));
       } else {
         pstmt.setNull(5, Types.BIGINT);
       }
-
       if (item.has("accepted_answer_id")) {
         pstmt.setLong(6, item.getLong("accepted_answer_id"));
       } else {
         pstmt.setNull(6, Types.BIGINT);
       }
-
       if (item.has("answer_count")) {
         pstmt.setLong(7, item.getLong("answer_count"));
       } else {
         pstmt.setNull(7, Types.BIGINT);
-      };
-
+      }
       if (item.has("last_activity_date")) {
         pstmt.setLong(8, item.getLong("last_activity_date"));
       } else {
         pstmt.setNull(8, Types.BIGINT);
-      };
-
+      }
       if (item.has("creation_date")) {
         pstmt.setLong(9, item.getLong("creation_date"));
       } else {
         pstmt.setNull(9, Types.BIGINT);
       }
-
       if (item.has("last_edit_date")) {
         pstmt.setLong(10, item.getLong("last_edit_date"));
       } else {
@@ -159,13 +337,11 @@ public class StackOverflowDataCollector {
       } else {
         pstmt.setNull(12, Types.VARCHAR);
       }
-
       if (item.has("link")) {
         pstmt.setString(13, item.getString("link"));
       } else {
         pstmt.setNull(13, Types.VARCHAR);
       }
-
       if (item.has("title")) {
         pstmt.setString(14, item.getString("title"));
       } else {
@@ -173,81 +349,11 @@ public class StackOverflowDataCollector {
       }
       pstmt.executeUpdate();
     } catch (Exception e) {
-      System.out.println("----------------skipped----------------");
+      System.out.println("----------------question skipped----------------");
       return;
     }
   }
 }
 
-/**
- * 建表语句：
- *
- * create table question
- * (
- *     tags               varchar,
- *     owner              varchar,
- *     is_answered        bool,
- *     view_count         bigint,
- *     protected_date     bigint,
- *     accepted_answer_id bigint,
- *     answer_count       bigint,
- *     last_activity_date bigint,
- *     creation_date      bigint,
- *     last_edit_date     bigint,
- *     question_id        bigint primary key,
- *     content_license    varchar,
- *     link               varchar,
- *     title              varchar
- * );
- */
 
-/**
- * 爬取的questions中的数据示例：
- * https://api.stackexchange.com/2.3/questions?page=1&pagesize=100&order=desc&sort=votes&tagged=java&site=stackoverflow
- *
- * {"tags":["java","c++","performance","cpu-architecture","branch-prediction"],
- * "owner":{"account_id":31692,
- *          "reputation":491761,
- *          "user_id":87234,
- *          "user_type":"registered",
- *          "accept_rate":100,
- *          "profile_image":"https://i.stack.imgur.com/FkjBe.png?s=256&g=1",
- *          "display_name":"GManNickG",
- *          "link":"https://stackoverflow.com/users/87234/gmannickg"},
- * "is_answered":true,
- * "view_count":1810966,
- * "protected_date":1399067470,
- * "accepted_answer_id":11227902,
- * "answer_count":25,"score":26979,
- * "last_activity_date":1683824982,
- * "creation_date":1340805096,
- * "last_edit_date":1683824982,
- * "question_id":11227809,
- * "content_license":"CC BY-SA 4.0",
- * "link":"https://stackoverflow.com/questions/11227809/why-is-processing-a-sorted-array-faster-than-processing-an-unsorted-array",
- * "title":"Why is processing a sorted array faster than processing an unsorted array?"},
- *
- *
- * 爬取的post的数据示例
- * https://api.stackexchange.com/2.3/posts/12345/comments?site=stackoverflow
- *
- * {"items":[{"owner":{"account_id":31692,
- *            "reputation":491771,
- *            "user_id":87234,
- *            "user_type":"registered",
- *            "accept_rate":100,
- *            "profile_image":"https://i.stack.imgur.com/FkjBe.png?s=256&g=1",
- *            "display_name":"GManNickG",
- *            "link":"https://stackoverflow.com/users/87234/gmannickg"},
- * "score":26980,
- * "last_edit_date":1683824982,
- * "last_activity_date":1683824982,
- * "creation_date":1340805096,
- * "post_type":"question",
- * "post_id":11227809,
- * "content_license":"CC BY-SA 4.0",
- * "link":"https://stackoverflow.com/q/11227809"}],
- * "has_more":false,
- * "quota_max":300,
- * "quota_remaining":299}
- */
+
