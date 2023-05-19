5363b7072d658627080c7e6652259fcd7637c8ce
qwq-y <2090078073@qq.com>
2023-05-19 14:03:03 +0800

    不稳定版，先存个档

---
+++ src/main/java/com/example/utils/Temp.java
@@ -0,0 +1,12 @@
+package com.example.utils;
+
+public class Temp {
+
+  public static void main(String[] args) {
+    long a = 12010541;
+    System.out.println(a % 13);
+    System.out.println(a % 17);
+    System.out.println(a % 19);
+  }
+
+}
