f6c287a63d5bb2847b18b0b4d1b7ad471a73bd06
TANGHULU6 <12110502@mail.sustech.edu.cn>
2023-05-23 16:56:51 +0800

    跨域，加依赖

--- src/main/java/com/example/Java2ProjApplication.java
+++ src/main/java/com/example/Java2ProjApplication.java
@@ -2,6 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Java2ProjApplication {
@@ -9,5 +12,16 @@ public class Java2ProjApplication {
   public static void main(String[] args) {
     SpringApplication.run(Java2ProjApplication.class, args);
   }
-
+  @Configuration
+  public class CrosConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**")
+              .allowedOriginPatterns("*")
+              .allowedMethods("GET","HEAD","POST","DELETE","OPTIONS","PUT")
+              .allowCredentials(true)
+              .maxAge(3600)
+              .allowedHeaders("*");
+    }
+  }
 }
